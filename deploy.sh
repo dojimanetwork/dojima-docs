0e4f9bdea8f02f8014bdbf83311d64f134ebe1d6
viswa3760 <viswa.bharath@dojima.network>
2022-08-13 23:22:36 +0530

    Docker Code Refctoring

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build --platform=linux/amd64 -f Dockerfile --force-rm  -t dojima-docs .
+docker build --platform=linux/amd64 -f Dockerfile --force-rm  -t developer-docs .
 
-docker tag dojima-docs:latest 576263512135.dkr.ecr.$REGION.amazonaws.com/core/docs:$TAG
+docker tag developer-docs:latest 576263512135.dkr.ecr.$REGION.amazonaws.com/core/docs:$TAG
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin 576263512135.dkr.ecr.$REGION.amazonaws.com
 
