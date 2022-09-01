9f51e8c33250de1c04cdedafaa2dddaeb7cb3b0a
viswa3760 <viswa.bharath@dojima.network>
2022-09-01 23:47:32 +0530

    Change in Docusaurus folder name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,8 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          path:'developer-docs',
+          routeBasePath: 'developer-docs',
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl: "https://github.com/dojimanetwork/dojima-docs.git",
@@ -65,15 +67,7 @@ const config = {
       footer: {
         style: "dark",
         links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
+
           {
             title: "Community",
             items: [
